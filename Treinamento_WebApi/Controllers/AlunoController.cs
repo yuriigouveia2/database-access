4725f75ceb55289aefe506a5d2e493eda21923b6
Yuri Gouveia <yuri.gouveia@eng.ci.ufpb.br>
2017-09-06 18:00:19 -0300

    DB Access

---
+++ Treinamento_WebApi/Controllers/AlunoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Treinamento_WebApi.Dockers;
+
+namespace Treinamento_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AlunoController : Controller
+    {
+        private FaculdadeContext _context;
+        public AlunoController(FaculdadeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Aluno> Get()
+        {
+            return _context.Alunos.ToList();
+        }
+
+        public Aluno Get(int id)
+        {
+            return _context.Alunos.Where(x => x.Id == id).SingleOrDefault();
+        }
+
+        [HttpPost]
+        public Aluno Post([FromBody]Aluno aluno)
+        {
+            _context.Alunos.Add(aluno);
+            _context.SaveChanges();
+
+            return aluno;
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Aluno aluno)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Treinamento_WebApi/Dockers/FaculdadeContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Treinamento_WebApi.Dockers
+{
+    public class FaculdadeContext : DbContext
+    {
+        public FaculdadeContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+
+    [Table("Aluno")]
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Matricula { get; set; }
+
+        public string Email { get; set; }
+    }
+}
